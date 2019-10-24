38fae8f5fe4faa9d069e752f377c8ec232898f4e
NashReptyl <poke.bref@gmail.com>
2019-10-24 14:46:27 +0200

    Derniere version pom

--- src/main/java/deqo/nmou/MySimpleStack/SimpleStackImpl.java
+++ src/main/java/deqo/nmou/MySimpleStack/SimpleStackImpl.java
@@ -1,28 +1,30 @@
 package deqo.nmou.MySimpleStack;
 
+import java.util.EmptyStackException;
+import java.util.Stack;
+
 public class SimpleStackImpl implements SimpleStack {
 
-    Stack<Object> wrappedStack = new Stack<Object>;
+    Stack<Object> wrappedStack = new Stack<Object>();
 
-    public ​​boolean​ isEmpty(){
+    public boolean isEmpty(){
         return wrappedStack.isEmpty();
     }
 
-    public​​ int​ getSize(){
+    public int getSize(){
         return wrappedStack.size();
     }
 
-
-    ​public ​​void​ push(​Item​ item){
+    public void push (Item item){
         wrappedStack.push(item);
     }
 
-    ​public ​​Item​ peek() ​throws​​ EmptyStackException​{
+    public Item peek() throws EmptyStackException {
         return (Item) wrappedStack.peek();
     }
 
 
-    public ​​Item​ pop() ​throws ​​EmptyStackException​{
+    public Item pop()  throws EmptyStackException{
         return (Item) wrappedStack.pop();
     }
 
